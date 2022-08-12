e0d33bd86f7e7b5c19d5b77f9e85e4cacfd94ebe
shahbuland <smatiana1@gmail.com>
2022-08-12 12:18:55 -0400

    Create sentiment pipeline and restructure rollout storage as a pipeline

--- framework/pipeline/__init__.py
+++ framework/pipeline/__init__.py
@@ -1,16 +1,36 @@
 from torch.utils.data import Dataset
 from datasets import load_from_disk
+import random
 
-from framework.data import GeneralElement
+from typing import Iterable, Any
+from abc import abstractmethod
+
+from framework.data import GeneralElement, RLElement
 
 class BasePipeline(Dataset):
     def __init__(self, path : str = "dataset"):
-        dataset = load_from_disk(path)
-    
-        # TODO
+        pass
 
     def __getitem__(self, index : int) -> GeneralElement:
         pass
 
     def __len__(self) -> int:
         pass
+
+class BaseRolloutStore(Dataset):
+    def __init__(self, capacity = -1):
+        self.history : Iterable[Any] = None
+        self.capacity = capacity
+    
+    @abstractmethod
+    def push(self, exps):
+        pass
+
+    def __getitem__(self, index : int) -> RLElement:
+        return self.history[index]
+
+    def __len__(self) -> int:
+        return len(self.history)
+
+
+    
---
+++ framework/pipeline/sentiment.py
@@ -0,0 +1,50 @@
+from datasets import load_dataset
+from functools import partial, reduce
+from typing import Tuple, Iterable
+from torchtyping import TensorType
+
+import torch
+
+from framework.pipeline import BasePipeline, BaseRolloutStore
+from framework.data.sentiment import SentimentGeneralElement, SentimentRLElement
+
+def filter_outliers(x):
+    return len(x['review']) > 200
+
+def process_data(dataset):
+    dataset = dataset.rename_columns({'text': 'review', 'label': 'sentiment'})
+    dataset = dataset.filter(filter_outliers, batched=False)
+    dataset = dataset
+    return dataset
+
+class SentimentPipeline(BasePipeline):
+    def __init__(self, path = None):
+        ds_train = load_dataset('imdb', split='train+test')
+        ds_train = process_data(ds_train)
+        self.text = ds_train["text"]
+
+    def __get__item(self, index : int) -> SentimentGeneralElement:
+        return SentimentGeneralElement(self.text[index])
+    
+    def __len__(self) -> int:
+        return len(self.text)
+
+class SentimentRolloutStorage(BaseRolloutStore):
+    def __init__(self):
+        super().__init__()
+
+        self.history : Iterable[Iterable[str], TensorType["N"]] = [[], torch.empty(0)]
+    
+    def push(self, exps : Tuple[Iterable[str], TensorType["N"]]):
+        txt, r = exps
+        self.history[0] += txt
+        self.history[1] = torch.cat((self.history[1], r))
+
+    def __getitem__(self, index : int) -> SentimentRLElement:
+        return SentimentRLElement(
+            self.history[0][index],
+            self.history[1][index]
+        )
+
+    def __len__(self) -> int:
+        return len(self.history)
