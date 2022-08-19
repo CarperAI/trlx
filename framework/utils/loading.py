7cbe41235b7ea640b9d09d4cddcccb743f9f4bb1
shahbuland <smatiana1@gmail.com>
2022-08-19 13:11:52 -0400

    Added utility for loading objects

---
+++ framework/utils/loading.py
@@ -0,0 +1,35 @@
+from typing import Callable
+
+from framework.pipeline import _DATAPIPELINE
+from framework.orchestrator import _ORCH
+from framework.model import _MODELS
+
+def get_model(name : str) -> Callable:
+    """
+    Return constructor for specified model
+    """
+    name = name.lower()
+    if name in _MODELS:
+        return _MODELS[name]
+    else:
+        raise Exception("Error: Trying to access a model that has not been registered")
+
+def get_pipeline(name : str) -> Callable:
+    """
+    Return constructor for specified pipeline
+    """
+    name = name.lower()
+    if name in _DATAPIPELINE:
+        return _DATAPIPELINE[name]
+    else:
+        raise Exception("Error: Trying to access a pipeline that has not been registered")
+
+def get_orchestrator(name : str) -> Callable:
+    """
+    Return constructor for specified orchestrator
+    """
+    name = name.lower()
+    if name in _ORCH:
+        return _ORCH[name]
+    else:
+        raise Exception("Error: Trying to access an orchestrator that has not been registered")
