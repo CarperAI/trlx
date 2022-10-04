993c2f9ac6b46c041480dab63d45a59520e61011
reciprocated <56548574+reciprocated@users.noreply.github.com>
2022-10-04 23:34:48 +0300

    stage ilql

---
+++ examples/ilql_sentiments.py
@@ -0,0 +1,53 @@
+from trlx.orchestrator.offline_orchestrator import OfflineOrchestrator
+from trlx.model.accelerate_ilql_model import ILQLModel
+from trlx.data.configs import TRLConfig
+
+from transformers import AutoTokenizer, pipeline
+from datasets import load_dataset
+
+import math
+import torch
+import numpy as np
+from typing import List, Iterable, Callable
+
+def batch_map(fn: Callable, xs: Iterable, bsize: int, desc=None):
+    out = []
+    for ind in range(math.ceil(len(xs) / bsize)):
+        batch = xs[ind*bsize:min(len(xs), (ind+1)*bsize)]
+        out.extend(fn(batch))
+
+    return out
+
+if __name__ == '__main__':
+    config = TRLConfig.load_yaml('configs/ilql_config.yml')
+    sentiment_pipe = pipeline('sentiment-analysis', 'lvwerra/distilbert-imdb', device=torch.device(0))
+
+    gpt_config_or_path = 'gpt2'
+    tokenizer = AutoTokenizer.from_pretrained(gpt_config_or_path)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    def reward_fn(samples: List[str]) -> List[float]:
+        if isinstance(samples[0], torch.Tensor):
+            samples = tokenizer.batch_decode(samples, skip_special_tokens=True)
+
+        sentiments = batch_map(lambda batch: sentiment_pipe(batch), samples, bsize=1024)
+        return [1-s['score'] if s['label'] == 'NEGATIVE' else s['score'] for s in sentiments]
+
+    model = ILQLModel(
+        config=config,
+        gpt_config_or_path=gpt_config_or_path,
+        tokenizer=tokenizer
+    )
+
+    n_prompts = 128
+    eval_prompts = torch.tensor([model.tokenizer.bos_token_id] * n_prompts).view(n_prompts, 1)
+    train_samples = load_dataset('imdb', split='train+test')['text']
+
+    orch = OfflineOrchestrator(
+        model,
+        train_samples,
+        eval_prompts,
+        reward_fn
+    )
+
+    model.learn()
---
+++ trlx/data/ilql_types.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from typing import Iterable, Callable
+from torchtyping import TensorType
+TensorType
+
+@dataclass
+class ILQLElement:
+    input_ids : TensorType["query_size"]
+    attention_mask : TensorType["query_size"]
+    rewards : TensorType["reward_size"]
+
+@dataclass
+class ILQLBatch:
+    input_ids : TensorType["batch_size", "query_size"]
+    attention_mask : TensorType["batch_size", "query_size"]
+    rewards : TensorType["batch_size", "reward_size"]
