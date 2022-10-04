e217a6831829165d4bff29b91aeaa36483d11bc3
Dahoas <alexdahoas@gmail.com>
2022-10-04 00:49:43 +0000

    made reporting accelerate config optional

--- trlx/model/accelerate_base_model.py
+++ trlx/model/accelerate_base_model.py
@@ -37,10 +37,11 @@ class AccelerateRLModel(BaseRLModel):
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.tokenizer.padding_side = 'left'
 
-        with open(self.config.train.accelerate_config_path, mode="r") as file:
-            accelerate_config = yaml.safe_load(file)
-        config_dict = self.config.to_dict()
-        config_dict.update(accelerate_config)
+        if self.config.train.accelerate_config_path != "":
+            with open(self.config.train.accelerate_config_path, mode="r") as file:
+                accelerate_config = yaml.safe_load(file)
+            config_dict = self.config.to_dict()
+            config_dict.update(accelerate_config)
         # TODO(dahoas): might need to move this
         self.accelerator = Accelerator(log_with='wandb')
         self.accelerator.init_trackers(self.config.train.project_name, config=config_dict)
