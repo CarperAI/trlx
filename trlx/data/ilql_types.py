35d34463539339059e3524c052c6c176a16e4774
Louis Castricato <wirezapp@gmail.com>
2022-10-05 16:44:53 -0400

    Adds style file and reward function capabilities to ppo orchestrator  (#8)
    * Moving reward fn to external
    
    * Added style file. Added reward_fn to ppo_orchestrator
    
    Co-authored-by: LouisCastricato <louis@stability.ai>

--- trlx/data/ilql_types.py
+++ trlx/data/ilql_types.py
@@ -1,17 +1,20 @@
 from dataclasses import dataclass
+from typing import Callable, Iterable
 
-from typing import Iterable, Callable
 from torchtyping import TensorType
+
 TensorType
+
 
 @dataclass
 class ILQLElement:
-    input_ids : TensorType["query_size"]
-    attention_mask : TensorType["query_size"]
-    rewards : TensorType["reward_size"]
+    input_ids: TensorType["query_size"]
+    attention_mask: TensorType["query_size"]
+    rewards: TensorType["reward_size"]
+
 
 @dataclass
 class ILQLBatch:
-    input_ids : TensorType["batch_size", "query_size"]
-    attention_mask : TensorType["batch_size", "query_size"]
-    rewards : TensorType["batch_size", "reward_size"]
+    input_ids: TensorType["batch_size", "query_size"]
+    attention_mask: TensorType["batch_size", "query_size"]
+    rewards: TensorType["batch_size", "reward_size"]
