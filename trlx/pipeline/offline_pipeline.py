f601203ccabbd50260a1935d3429955abc054272
reciprocated <56548574+reciprocated@users.noreply.github.com>
2022-10-10 19:00:44 +0300

    fix(ilql): sampling on variable sized prompts & stage simplified api

--- trlx/pipeline/offline_pipeline.py
+++ trlx/pipeline/offline_pipeline.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+from functools import partial
 from typing import Callable, Iterable, Tuple
 
 import torch
@@ -5,6 +7,7 @@ from datasets import load_dataset
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader
 from torchtyping import TensorType
+from transformers import DataCollatorWithPadding
 
 from trlx.data.ilql_types import ILQLBatch, ILQLElement
 from trlx.pipeline import BasePipeline, BaseRolloutStore, register_datapipeline
@@ -12,18 +15,25 @@ from trlx.pipeline import BasePipeline, BaseRolloutStore, register_datapipeline
 
 @register_datapipeline
 class OfflinePipeline(BasePipeline):
-    def __init__(self, texts=None):
+    def __init__(self, tokenizer, tokenized):
         super().__init__()
-        self.texts = texts
+        self.tokenizer = deepcopy(tokenizer)
+        if self.tokenizer:
+            self.tokenizer.padding_side = "left"
 
-    def __getitem__(self, index: int):
-        return self.texts[index]
+        self.tokenized = tokenized
+
+    def __getitem__(self, ix: int):
+        return self.tokenized[ix]
 
     def __len__(self) -> int:
-        return len(self.texts)
+        return len(self.tokenized)
 
-    def create_loader(self, batch_size: int, shuffle: bool) -> DataLoader:
-        return DataLoader(self, batch_size=batch_size, shuffle=shuffle)
+    def create_loader(self, batch_size: int) -> DataLoader:
+        collate_fn = (
+            DataCollatorWithPadding(self.tokenizer) if self.tokenizer else torch.vstack
+        )
+        return DataLoader(self, batch_size=batch_size, collate_fn=collate_fn)
 
 
 class OfflineRolloutStorage(BaseRolloutStore):
@@ -34,27 +44,25 @@ class OfflineRolloutStorage(BaseRolloutStore):
         self.attention_mask = attention_mask
         self.rewards = rewards
 
-    def __getitem__(self, index: int) -> ILQLElement:
+    def __getitem__(self, ix: int) -> ILQLElement:
         return ILQLElement(
-            self.input_ids[index], self.attention_mask[index], self.rewards[index]
+            self.input_ids[ix], self.attention_mask[ix], self.rewards[ix]
         )
 
     def __len__(self) -> int:
         return len(self.input_ids)
 
-    def create_loader(self, batch_size: int, eos_token_id=0):
+    def create_loader(self, batch_size: int):
         def collate_fn(elems: Iterable[ILQLElement]):
             return ILQLBatch(
                 pad_sequence(
-                    [x.input_ids for x in elems],
-                    batch_first=True,
-                    padding_value=eos_token_id,
+                    [x.input_ids for x in elems], batch_first=True, padding_value=0
                 ),
                 pad_sequence(
                     [x.attention_mask for x in elems], batch_first=True, padding_value=0
                 ),
                 pad_sequence(
-                    [x.rewards for x in elems], batch_first=True, padding_value=0
+                    [x.rewards for x in elems], batch_first=True, padding_value=0.0
                 ),
             )
 
