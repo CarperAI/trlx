7447bedc78da1652dc22c9cdcc596d1dd7320888
ayulockin <mein2work@gmail.com>
2022-10-26 13:22:13 +0000

    req + sweep config + sweep file + config done

---
+++ train_sweep.py
@@ -0,0 +1,140 @@
+import argparse
+
+from trlx.data.configs import TRLConfig
+
+import ray
+from ray.air import session
+from ray import tune
+
+import wandb
+from ray.air.callbacks.wandb import WandbLoggerCallback
+
+
+def get_strategy(value):
+    strategy = value["strategy"]
+    if strategy=="uniform":
+        assert isinstance(value["bounds"], list)
+        assert len(value["bounds"])==2
+        return tune.uniform(*value["bounds"])
+    elif strategy=="quniform":
+        assert isinstance(value["bounds"], list)
+        assert len(value["bounds"])==3
+        return tune.quniform(*value["bounds"])
+    elif strategy=="loguniform":
+        assert isinstance(value["bounds"], list)
+        assert len(value["bounds"])==3
+        return tune.loguniform(*value["bounds"])
+    elif strategy=="qloguniform":
+        assert isinstance(value["bounds"], list)
+        assert len(value["bounds"])==4
+        return tune.qloguniform(*value["bounds"])
+    elif strategy=="randn":
+        assert isinstance(value["bounds"], list)
+        assert len(value["bounds"])==2
+        return tune.randn(*value["bounds"])
+    elif strategy=="qrandn":
+        assert isinstance(value["bounds"], list)
+        assert len(value["bounds"])==3
+        return tune.qrandn(*value["bounds"])
+    elif strategy=="randint":
+        assert isinstance(value["bounds"], list)
+        assert len(value["bounds"])==2
+        return tune.randint(*value["bounds"])
+    elif strategy=="qrandint":
+        assert isinstance(value["bounds"], list)
+        assert len(value["bounds"])==3
+        return tune.qrandint(*value["bounds"])
+    elif strategy=="lograndint":
+        assert isinstance(value["bounds"], list)
+        assert len(value["bounds"])==3
+        return tune.lograndint(*value["bounds"])
+    elif strategy=="qlograndint":
+        assert isinstance(value["bounds"], list)
+        assert len(value["bounds"])==4
+        return tune.qlograndint(*value["bounds"])
+    elif strategy=="choice":
+        assert isinstance(value["choices"], list)
+        return tune.choice(value["choices"])
+    elif strategy=="grid":
+        assert isinstance(value["values"], list)
+        return tune.grid_search(value["values"])
+
+
+def parse_config(config: dict):
+    def parse_param_space(param_space: dict):
+        for k, v in param_space.items():
+            if isinstance(v, dict):
+                if "strategy" in v.keys():
+                    strategy = get_strategy(v)
+                    param_space[k] = strategy
+
+        return param_space
+
+    model_dict = parse_param_space(config["model"])
+    train_dict = parse_param_space(config["train"])
+    method_dict = parse_param_space(config["method"])
+
+    return {
+        "model": model_dict,
+        "train": train_dict,
+        "method": method_dict
+    }
+
+
+def train_function(config):
+    config = TRLConfig.from_dict(config)
+    print(config)
+
+
+def tune_function(train_function, param_space: dict):
+    tuner = tune.Tuner(
+        tune.with_resources(train_function, resources={"cpu": 2, "gpu": 1}),
+        param_space=param_space,
+    )
+
+    results = tuner.fit()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config", type=str, default=None, required=True, help="The config file defining the param_space."
+    )
+    parser.add_argument(
+        "--num-cpus", type=int, default=4, help="Number of CPUs to use."
+    )
+    parser.add_argument(
+        "--num-gpus", type=int, default=0, help="Number of GPUs to use."
+    )
+    parser.add_argument(
+        "--smoke-test", action="store_true", help="Finish quickly for testing"
+    )
+    parser.add_argument(
+        "--server-address",
+        type=str,
+        default=None,
+        required=False,
+        help="The address of server to connect to if using Ray Client.",
+    )
+
+    args, _ = parser.parse_known_args()
+
+    # Read config and parse it
+    config = TRLConfig.load_yaml(args.config).to_dict()
+    config = parse_config(config)
+    print(config)
+
+    # Initialize Ray.
+    if args.smoke_test:
+        ray.init(num_cpus=args.num_cpus, num_gpus=args.num_gpus)
+    elif args.server_address:
+        ray.init(
+            num_cpus=args.num_cpus,
+            num_gpus=args.num_gpus,
+            address=f"ray://{args.server_address}"
+        )
+
+    tune_function(train_function, config)
+
+    # Shut down Ray.
+    ray.shutdown()
