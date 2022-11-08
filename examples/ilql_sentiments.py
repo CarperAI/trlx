556cac1e7c10de166b76dd9d58d01136bd7c3c6c
reciprocated <56548574+reciprocated@users.noreply.github.com>
2022-11-08 20:46:50 +0200

    chore(sentiments): speed up examples
    put pipeline on gpu + batch_size

--- examples/ilql_sentiments.py
+++ examples/ilql_sentiments.py
@@ -3,20 +3,24 @@
 from datasets import load_dataset
 from transformers import pipeline
 
+import os
 import trlx
 
 if __name__ == "__main__":
-    sentiment_fn = pipeline("sentiment-analysis", "lvwerra/distilbert-imdb")
+    sentiment_fn = pipeline(
+        "sentiment-analysis",
+        "lvwerra/distilbert-imdb",
+        device=int(os.environ.get("LOCAL_RANK", 0)),
+    )
 
     def metric_fn(samples):
-        outputs = sentiment_fn(samples, return_all_scores=True)
+        outputs = sentiment_fn(samples, return_all_scores=True, batch_size=256)
         sentiments = [output[1]["score"] for output in outputs]
         return {"sentiments": sentiments}
 
     imdb = load_dataset("imdb", split="train+test")
 
     trlx.train(
-        "gpt2",
         dataset=(imdb["text"], imdb["label"]),
         eval_prompts=["I don't know much about Hungarian underground"] * 64,
         metric_fn=metric_fn,
--- examples/ppo_sentiments.py
+++ examples/ppo_sentiments.py
@@ -3,14 +3,19 @@
 
 from datasets import load_dataset
 from transformers import pipeline
+import os
 
 import trlx
 
 if __name__ == "__main__":
-    sentiment_fn = pipeline("sentiment-analysis", "lvwerra/distilbert-imdb", device=-1)
+    sentiment_fn = pipeline(
+        "sentiment-analysis",
+        "lvwerra/distilbert-imdb",
+        device=int(os.environ.get("LOCAL_RANK", -1)),
+    )
 
     def reward_fn(samples):
-        outputs = sentiment_fn(samples, return_all_scores=True)
+        outputs = sentiment_fn(samples, return_all_scores=True, batch_size=256)
         sentiments = [output[1]["score"] for output in outputs]
         return sentiments
 
@@ -22,5 +27,5 @@ if __name__ == "__main__":
         "lvwerra/gpt2-imdb",
         reward_fn=reward_fn,
         prompts=prompts,
-        eval_prompts=["I don't know much about Hungarian underground"] * 64,
+        eval_prompts=["I don't know much about Hungarian underground"] * 128,
     )
