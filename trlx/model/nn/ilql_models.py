e1a0753f304dc269b404c02695c557ca61200b40
crumb <58605641+aicrumb@users.noreply.github.com>
2022-12-12 13:23:52 -0600

    Messed up my plan for dtype, whoops, fixing

--- trlx/model/nn/ilql_models.py
+++ trlx/model/nn/ilql_models.py
@@ -186,7 +186,6 @@ class CausalLMWithValueHeads(nn.Module):
     def __init__(
         self,
         config: Union[transformers.PretrainedConfig, str],
-        torch_dtype: str = "float32",
         ilql_config: ILQLConfig,
         num_layers_unfrozen=-1,
     ):
@@ -206,7 +205,7 @@ class CausalLMWithValueHeads(nn.Module):
         
         self.base_model = transformers.AutoModelForCausalLM.from_pretrained(
             self.config.name_or_path,
-            torch_dtype=torch_dtype
+            torch_dtype=self.config.model.dtype
         )
         
         self.base_model.transformer = hf_get_causal_base_model(self.base_model)
