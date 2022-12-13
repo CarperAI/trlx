1ec1232d6613f1e46c4a65231a81c2f6f9ab22b3
crumb <58605641+aicrumb@users.noreply.github.com>
2022-12-13 01:21:49 -0600

    Use accelerator gradient accumulation
    Still have to add the gradient_accumulation_steps to the initialization of the Accelerator

--- trlx/model/accelerate_base_model.py
+++ trlx/model/accelerate_base_model.py
@@ -249,16 +249,16 @@ class AccelerateRLModel(BaseRLModel):
         for _ in range(self.config.train.epochs):
             for batch in self.train_dataloader:
                 for _ in range(self.n_updates_per_batch):
-                    forward_time = time()
-                    loss, stats = self.loss(batch)
-                    loss = loss / self.config.train.gradient_accumulation_steps
-                    forward_time = time() - forward_time
+                    with self.accelerator.accumulate(self.model):
+                        forward_time = time()
+                        loss, stats = self.loss(batch)
+                        loss = loss / self.config.train.gradient_accumulation_steps
+                        forward_time = time() - forward_time
 
-                    backward_time = time()
-                    self.accelerator.backward(loss)
-                    backward_time = time() - backward_time
+                        backward_time = time()
+                        self.accelerator.backward(loss)
+                        backward_time = time() - backward_time
 
-                    if self.iter_count % self.config.train.gradient_accumulation_steps == 0:
                         self.opt.step()
                         self.opt.zero_grad()
                         self.scheduler.step()
