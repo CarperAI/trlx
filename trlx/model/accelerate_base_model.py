fix(base_model): pin rewards to single precision

trlx/model/accelerate_base_model.py
@@ -202,9 +202,7 @@ class AccelerateRLModel(BaseRLModel):
 
             # in online setting, compute the reward for validation
             if self.reward_fn:
-                rewards = torch.as_tensor(
-                    self.reward_fn(str_samples), dtype=torch.float
-                )
+                rewards = torch.tensor(self.reward_fn(str_samples), dtype=torch.float)
                 mean_reward = rewards.mean()
                 columns.append("reward")
                 columns_data.append(rewards)

trlx/orchestrator/ppo_orchestrator.py
@@ -79,7 +79,9 @@ class PPOOrchestrator(Orchestrator):
                 samples, skip_special_tokens=True
             )
             exp_score_time = time()
-            scores = torch.tensor(self.score(texts), device=samples.device)
+            scores = torch.tensor(
+                self.score(texts), device=samples.device, dtype=torch.float
+            )
             stats["exp_score_time"] = time() - exp_score_time
 
             # store statistics of the initial rollout as reference