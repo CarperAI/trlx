Fix context overflow (#131)
* feat(ppo_models): context truncating generation

* feat(base_model): segragate prompts and responses in logging

* fix(ppo_model): truncate left padded tokens

* revert(ppo_models): remove context overflowing generate()

* feat(configs): add max_new_tokens

* fix(pipeline): truncate prompts

* chore(base_model): remove whole samples from logging

* chore(configs): update the rest of configs

* fix(configs): update program synthesis config

* fix(base_model): prompts sizes

* revert(config): emulate old ppo_sentiments behavior

* fix(ppo): unequal generation lengths

* chore(ppo): put indexing on cpu

* revert(configl): old ilql_sentiments behavior

* fix(ppo): unzero clipfrac

* merge(configs): delete old options

* revert(config): old ppo_sentiment behavior

* refactor(ppo_orchestrator): remove unused ref_logprobs

* fix(base_model): pin rewards to single precision

* refactor(ppo): rename padding percentage

* feat(wandb): add git branch name to tags

* refactor(wandb): logging name hierarchy

* revert(wandb): merge tags into a single string

trlx/model/nn/ilql_models.py
@@ -42,8 +42,8 @@ class ILQLConfig(MethodConfig):
     awac_scale: float
     alpha: float
     steps_for_target_q_sync: float
-    betas: Sequence[float]
     two_qs: bool
+    gen_kwargs: dict
 
     def heads(self, hidden_size: int, vocab_size: int):
         return ILQLHeads(self, hidden_size, vocab_size)
@@ -140,7 +140,6 @@ class ILQLHeads(nn.Module):
         states_ixs: torch.Tensor = None,
         actions_ixs: torch.Tensor = None,
     ):
-
         if states_ixs is not None:
             states_hs = hs.gather(
                 dim=1, index=states_ixs.unsqueeze(-1).repeat(1, 1, hs.shape[-1])
@@ -260,7 +259,8 @@ class CausalLMWithValueHeads(nn.Module):
         position_ids=None,
         past_key_values=None,
         beta=1,
-        max_length=32,
+        max_new_tokens=32,
+        max_length=1024,
         temperature=1,
         top_k=20,
         logit_mask=None,
@@ -278,13 +278,12 @@ class CausalLMWithValueHeads(nn.Module):
             position_ids.masked_fill_(attention_mask.eq(0), 0)
 
         samples = input_ids.clone()
-        tensors = defaultdict(list)
-        n_new_tokens = max_length - input_ids.shape[1]
+        max_new_tokens = min(max_new_tokens, max_length - input_ids.shape[1])
 
         finished = torch.zeros(
             input_ids.shape[0], 1, dtype=torch.long, device=input_ids.device
         )
-        for _ in range(n_new_tokens):
+        for _ in range(max_new_tokens):
             out = self.forward(
                 input_ids=input_ids,
                 attention_mask=attention_mask,