f3b8c63c855879c0daa910ca78a3bbd6b44f9205
Alexey Bukhtiyarov <a.bukhtiyarov@yandex.ru>
2022-12-22 21:18:20 +0300

    better name

--- trlx/trainer/accelerate_base_trainer.py
+++ trlx/trainer/accelerate_base_trainer.py
@@ -281,8 +281,8 @@ class AccelerateRLTrainer(BaseRLTrainer):
 
                     stats["time/forward"] = forward_time
                     stats["time/backward"] = backward_time
-                    for i, lr in enumerate(self.scheduler.get_last_lr()):
-                        stats[f"learning_rate/group_{i}"] = lr
+                    for group_number, lr in enumerate(self.scheduler.get_last_lr()):
+                        stats[f"learning_rate/group_{group_number}"] = lr
 
                     if self.iter_count % self.config.train.eval_interval == 0:
                         results = self.evaluate()
