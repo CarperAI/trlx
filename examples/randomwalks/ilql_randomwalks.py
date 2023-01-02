267c7f8c0be4bdbe5254b117e7ad34d5402fa769
reciprocated <56548574+reciprocated@users.noreply.github.com>
2023-01-02 18:50:15 +0200

    chore(configs): plug with beta

--- examples/randomwalks/ilql_randomwalks.py
+++ examples/randomwalks/ilql_randomwalks.py
@@ -17,7 +17,7 @@ def main(hparams={}):
     metric_fn, eval_prompts, walks, _ = generate_random_walks(seed=config.train.seed)
     rewards = metric_fn(walks)["optimality"]
     # split each random walk into (starting state, rest of the walk)
-    walks = [[walk[:1], walk[1:]] for w in walks]
+    walks = [[walk[:1], walk[1:]] for walk in walks]
 
     trlx.train(
         GPT2Config(n_layer=6, n_embd=144, vocab_size=23),
