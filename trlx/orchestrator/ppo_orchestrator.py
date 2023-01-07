fix ppo for causal models, add save best, seperate rollouts/eval args

trlx/orchestrator/ppo_orchestrator.py
@@ -146,32 +146,30 @@ class PPOOrchestrator(Orchestrator):
                             decoder_input_ids=response_tensors,
                         ).logits
             else:
-                (
-                    all_tokens,
-                    attention_mask,
-                    position_ids,
-                ) = self.trainer.get_model_inputs(
-                    query_tensors.to(response_tensors.device), response_tensors
+                all_tokens = torch.cat(
+                    (query_tensors.to(response_tensors.device), response_tensors), dim=1
+                )
+                attention_mask = (
+                    all_tokens.not_equal(self.trainer.tokenizer.pad_token_id)
+                    .long()
+                    .to(all_tokens.device)
                 )
                 with torch.no_grad():
                     logits, *_, values = self.trainer.model(
                         all_tokens,
                         attention_mask=attention_mask,
-                        position_ids=position_ids,
                     )
                     # TODO(dahoas): When hydra model works need to also support generation on hydra head
                     if hasattr(self.trainer.model, "frozen_head"):
                         ref_logits = self.trainer.model.forward_hydra(
                             all_tokens,
                             attention_mask=attention_mask,
-                            position_ids=position_ids,
                             return_dict=False,
                         )
                     else:
                         ref_logits, _, *_ = self.ref_model(
                             all_tokens,
                             attention_mask=attention_mask,
-                            position_ids=position_ids,
                             return_dict=False,
                         )
                         ref_logits = ref_logits.to(self.trainer.accelerator.device)
@@ -188,7 +186,6 @@ class PPOOrchestrator(Orchestrator):
                 ref_logprobs = logprobs_from_logits(
                     ref_logits[:, :-1, :], all_tokens[:, 1:]
                 )
-                values = values.cpu()[:, :-1]
 
             n = samples.shape[0]
             logprobs = logprobs.cpu()
@@ -209,9 +206,18 @@ class PPOOrchestrator(Orchestrator):
                     for ix in range(n)
                 ]
             else:
-                start = query_tensors.shape[1] - 1
+                n = samples.shape[0]
+                values = values.cpu()
+                logprobs = logprobs.cpu()
+                ref_logprobs = ref_logprobs.cpu()
+                query_tensors = query_tensors.cpu()
+                response_tensors = response_tensors.cpu()
+                start = query_tensors.shape[1]
                 ends = start + attention_mask[:, start:].sum(1)
-                all_values = [values[ix, start : ends[ix]] for ix in range(n)]
+                for ix in range(n):
+                    if ends[ix] == all_tokens.shape[1]:
+                        ends[ix] = ends[ix] - 1
+                all_values = [values[ix, start - 1 : ends[ix] - 1] for ix in range(n)]
                 all_logprobs = [logprobs[ix, start : ends[ix]] for ix in range(n)]
                 rewards = -self.trainer.kl_ctl.value * (logprobs - ref_logprobs)
 
@@ -226,7 +232,7 @@ class PPOOrchestrator(Orchestrator):
                 for ix in range(n):
                     rs = rewards[ix][start : ends[ix]]
                     if len(rs) == 0:  # fix for empty responses
-                        rs = torch.tensor([rewards[ix][start]])
+                        rs = torch.tensor([0.0])
                     rs[-1] = scores[ix]
                     all_rewards[ix] = rs
             new_ppo_rl_elements = [
@@ -239,12 +245,6 @@ class PPOOrchestrator(Orchestrator):
                 )
                 for i in range(n)
             ]
-            for element in new_ppo_rl_elements:
-                if len(element.rewards) != len(element.values):
-                    import ipdb
-
-                    ipdb.set_trace()
-
             ppo_rl_elements += new_ppo_rl_elements
             exp_time = clock.tick()
 

trlx/trainer/accelerate_ppo_trainer.py
@@ -145,22 +145,20 @@ class AcceleratePPOTrainer(AccelerateRLTrainer):
                 mask[:, start:end],
             )
         else:
-            tokens, attention_mask, position_ids = self.get_model_inputs(
-                query_tensors, response_tensors
+            tokens = torch.cat((query_tensors, response_tensors), dim=1)
+            attention_mask = (
+                tokens.not_equal(self.tokenizer.pad_token_id).long().to(tokens.device)
             )
             logits, *_, values_pred = self.model(
-                tokens, attention_mask=attention_mask, position_ids=position_ids
+                tokens,
+                attention_mask=attention_mask,
             )
-            values_pred = values_pred[:, :-1]
             logprobs = logprobs_from_logits(logits[:, :-1, :], tokens[:, 1:])
-            attention_mask = attention_mask[:, :-1]
-
-            # Only the response part of the values/logprobs is needed
-            start = query_tensors.shape[1] - 1
+            start = query_tensors.shape[1]
             end = start + response_length
             logprobs, values_pred, mask = (
                 logprobs[:, start:end],
-                values_pred[:, start:end],
+                values_pred[:, start - 1 : end - 1],
                 attention_mask[:, start:end],
             )
 