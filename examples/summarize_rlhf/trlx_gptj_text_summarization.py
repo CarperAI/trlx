7a96c2c2f5a47e122026d907f5e4d2273b8a103d
Duy Phung <duyphung@ip-172-31-45-16.ec2.internal>
2023-01-12 19:13:58 +0000

    refactor trlx ppo train file

--- examples/summarize_rlhf/trlx_gptj_text_summarization.py
+++ examples/summarize_rlhf/trlx_gptj_text_summarization.py
@@ -29,38 +29,10 @@ if __name__ == "__main__":
     rw_model.load_state_dict(torch.load(REWARD_CHECKPOINT_PATH))
     rw_model.half()
     rw_model.eval()
-    rw_device = torch.device("cuda:{}".format(1))
+    rw_device = torch.device("cuda:{}".format(1))  # set reward model device
     rw_model.to(rw_device)
 
-    def reward_fn(samples: List[str]):
-        original_samples = [text.split("TL;DR:")[0] + "TL;DR: " for text in samples]
-        original_samples = [
-            text + train_post_summ[text.strip()] for text in original_samples
-        ]
-
-        original_scores_list = []
-        batch_size = 2
-        for i in range(0, len(original_samples), batch_size):
-            sub_samples = original_samples[i : i + batch_size]
-            sub_samples = [
-                "<|startoftext|>" + chosen + "<|endoftext|>" for chosen in sub_samples
-            ]
-            encodings_dict = rw_tokenizer(
-                sub_samples,
-                truncation=True,
-                max_length=config.train.seq_length,
-                padding="max_length",
-                return_tensors="pt",
-            )
-            input_ids = encodings_dict["input_ids"].to(rw_device)
-            attn_masks = encodings_dict["attention_mask"].to(rw_device)
-            input_ids = input_ids.repeat(2, 1)
-            attn_masks = attn_masks.repeat(2, 1)
-            with torch.no_grad():
-                sub_scores = rw_model(input_ids=input_ids, attention_mask=attn_masks)
-            original_scores_list.append(sub_scores["chosen_end_scores"])
-        original_scores = torch.cat(original_scores_list, dim=0)
-
+    def get_scores(samples: List[str]):
         scores_list = []
         batch_size = 2
         for i in range(0, len(samples), batch_size):
@@ -83,6 +55,39 @@ if __name__ == "__main__":
                 sub_scores = rw_model(input_ids=input_ids, attention_mask=attn_masks)
             scores_list.append(sub_scores["chosen_end_scores"])
         scores = torch.cat(scores_list, dim=0)
+        return scores
+
+    def get_prompt_dataset(prompts, max_length):
+        """
+        Get the prompt after T5 decoding to make sure dictionary
+        of prompts and summaries is consistent decode prompt from trlX pipeline
+        """
+        formatted_prompts = []
+        for i in tqdm(range(len(prompts))):
+            tmp = tokenizer.decode(
+                tokenizer(
+                    prompts[i].split("TL;DR:")[0],
+                    truncation=True,
+                    max_length=max_length
+                    - 5,  # to make sure "TL;DR" dont get truncated
+                )["input_ids"],
+                skip_special_tokens=True,
+            ).strip()
+            tmp = tmp + "\nTL;DR:"
+            tmp = tokenizer.decode(
+                tokenizer(tmp, truncation=True, max_length=max_length)["input_ids"],
+                skip_special_tokens=True,
+            ).strip()
+            formatted_prompts.append(tmp)
+        return formatted_prompts
+
+    def reward_fn(samples: List[str]):
+        original_samples = [text.split("TL;DR:")[0] + "TL;DR: " for text in samples]
+        original_samples = [
+            text + post_summary_dict[text.strip()] for text in original_samples
+        ]
+        original_scores = get_scores(original_samples)
+        scores = get_scores(samples)
         norms_scores = scores - original_scores
         return norms_scores
 
@@ -91,7 +96,9 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(config.model.tokenizer_path)
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "left"
-    max_length = config.train.seq_length - config.method.gen_kwargs["max_new_tokens"]
+    max_length_input = (
+        config.train.seq_length - config.method.gen_kwargs["max_new_tokens"]
+    )
 
     dataset = load_dataset("CarperAI/openai_summarize_tldr")
 
@@ -100,50 +107,24 @@ if __name__ == "__main__":
     val_set = [(sample["prompt"], sample["label"]) for sample in dataset["valid"]]
 
     # Split contents into summaries and labels
-    train_openai_summ, train_labels = zip(*train_set)
-    val_openai_summ, val_labels = zip(*val_set)
+    train_posts, train_summaries = zip(*train_set)
+    val_posts, val_summaries = zip(*val_set)
 
-    train_post_summ = {}
-    train_prompts = []
-    for i in tqdm(range(len(train_openai_summ))):
-        tmp = tokenizer.decode(
-            tokenizer(
-                train_openai_summ[i].split("TL;DR:")[0],
-                truncation=True,
-                max_length=max_length - 5,
-            )["input_ids"],
-            skip_special_tokens=True,
-        ).strip()
-        tmp = tmp + "\nTL;DR:"
-        tmp = tokenizer.decode(
-            tokenizer(tmp, truncation=True, max_length=max_length)["input_ids"],
-            skip_special_tokens=True,
-        ).strip()
-        train_prompts.append(tmp)
-        train_post_summ[tmp] = train_labels[i]
-
-    val_prompts = []
-    for i in tqdm(range(len(val_openai_summ))):
-        tmp = tokenizer.decode(
-            tokenizer(
-                val_openai_summ[i].split("TL;DR:")[0],
-                truncation=True,
-                max_length=max_length - 5,
-            )["input_ids"],
-            skip_special_tokens=True,
-        ).strip()
-        tmp = tmp + "\nTL;DR:"
-        tmp = tokenizer.decode(
-            tokenizer(tmp, truncation=True, max_length=max_length)["input_ids"],
-            skip_special_tokens=True,
-        ).strip()
-        train_post_summ[tmp] = val_labels[i]
-        val_prompts.append(tmp)
+    # Get the OpenAI summaries
+    post_summary_dict = {}
+    train_prompts = get_prompt_dataset(train_posts, max_length_input)
+    for i in range(len(train_prompts)):
+        post_summary_dict[train_prompts[i]] = train_summaries[i]
+    val_prompts = get_prompt_dataset(val_posts, max_length_input)
+    for i in range(len(val_prompts)):
+        post_summary_dict[val_prompts[i]] = val_summaries[i]
 
     trainer = trlx.train(
         config.model.model_path,
         reward_fn=reward_fn,
         prompts=train_prompts,
-        eval_prompts=val_prompts[0:1000],
+        eval_prompts=val_prompts[
+            0:1000
+        ],  # sampling 1000 validation prompts for evaluation speed in training
         config=config,
     )
