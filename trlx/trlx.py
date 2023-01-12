cd4c70dd6229cd9b207badf38501fed379ca573b
reciprocated <56548574+reciprocated@users.noreply.github.com>
2023-01-12 18:40:12 +0200

    docs(trlx): update `train`'s signature

--- trlx/trlx.py
+++ trlx/trlx.py
@@ -1,5 +1,5 @@
 import os
-from typing import Callable, Iterable, List, Optional, Tuple
+from typing import Callable, Dict, Iterable, List, Optional, Tuple
 
 from trlx.data.configs import TRLConfig
 from trlx.utils import set_seed
@@ -8,11 +8,15 @@ from trlx.utils.loading import get_orchestrator, get_pipeline, get_trainer
 
 def train(
     model_path: Optional[str] = None,
-    reward_fn: Optional[Callable] = None,
+    reward_fn: Optional[
+        Callable[[List[str], List[str], List[str]], List[float]]
+    ] = None,
     dataset: Optional[Iterable[Tuple[str, float]]] = None,
     prompts: Optional[List[str]] = None,
     eval_prompts: Optional[List[str]] = None,
-    metric_fn: Optional[Callable] = None,
+    metric_fn: Optional[
+        Callable[[List[str], List[str], List[str]], Dict[str, List[float]]]
+    ] = None,
     config: Optional[TRLConfig] = None,
     logit_mask: Optional[List[List[bool]]] = None,
     stop_word: Optional[str] = "",
@@ -23,7 +27,9 @@ def train(
 
     Args:
         model_path (Optional[str]): Path to either huggingface checkpoint or a local directory
-        reward_fn (List[str] -> List[float]): Function to rate batches of generated samples
+        reward_fn (Optional[Callable[[List[str], List[str], List[str]], List[float]]]):
+            Function to rate batches of generated samples. Its arguments are
+            (`samples`, `prompts`, `outputs`) and the return is a list of `rewards` per each sample
         dataset (List[Union[str, List[str]]], List[float]):
             Lists of samples and rewards for offline training. Samples consist of a variable number
             of prompts (questions, environment states etc.) and outputs which are meant to be optimized.
@@ -31,10 +37,14 @@ def train(
             Giving a single string `s` for the sample is a shorthand for (`tokenizer.bos_token`, `s`)
         prompts (List[str]): Prompts to sample off from during online training
         eval_prompts (List[str]): Prompts to periodically validate training on
-        metric_fn (Optional[Callable[List[str], List[float]]]): Function to compute statistics on validation samples
+        metric_fn (Optional[Callable[[List[str], List[str], List[str]], Dict[str, List[float]]]]):
+            Function to compute statistics on batches of gnerated samples. Its arguments are the same
+            as in `reward_fn` (`samples`, `prompts`, `outputs`) but the return is dictionary with keys
+            as metric's name and values and lists of numeric values per each sample in batch
         config (Optional[TRLConfig]): TRL configuration object to override default settings
         logit_mask (Optional[List]): Bigram masking matrix
-        stop_word (Optional[str]): A string to trim generations (either for experience or evaluation) up to its encounter
+        stop_word (Optional[str]): A string to trim generations (either for experience or evaluation) up to
+            its encounter in them
     """
 
     if reward_fn is not None:
