9d8c101dff09063bad97342cadf6151bc8fa25a3
reciprocated <56548574+reciprocated@users.noreply.github.com>
2023-01-14 01:13:43 +0200

    feat(configs): add tokenizer options

--- trlx/trainer/accelerate_base_trainer.py
+++ trlx/trainer/accelerate_base_trainer.py
@@ -52,15 +52,12 @@ class AccelerateRLTrainer(BaseRLTrainer):
         self.opt = self.setup_optimizer()
         self.scheduler = self.setup_scheduler()
 
-        if config.model.tokenizer_path:
-            self.tokenizer = AutoTokenizer.from_pretrained(config.model.tokenizer_path)
-            self.tokenizer.padding_side = "left"
-            self.tokenizer.truncation_side = "right"
-            self.tokenizer.sep_token = "<sep>"
-            if config.model.model_arch_type != "seq2seq":
-                self.tokenizer.pad_token = self.tokenizer.eos_token
-        else:
-            self.tokenizer = None
+        self.tokenizer = AutoTokenizer.from_pretrained(config.tokenizer.tokenizer_path)
+        self.tokenizer.padding_side = config.tokenizer.padding_side
+        self.tokenizer.truncation_side = config.tokenizer.truncation_side
+        self.tokenizer.sep_token = "<sep>"
+        if config.model.model_arch_type != "seq2seq":
+            self.tokenizer.pad_token = self.tokenizer.eos_token
 
         script_name = os.path.basename(sys.argv[0]).rsplit(".", 1)[0]
         if not isinstance(config.model.model_path, str):
