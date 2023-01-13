16993649f6597b7d580ea49bb0f546bb5a5ddcf5
Jeremy Gillen <jez.gillen@gmail.com>
2023-01-13 20:02:12 +0000

    actually got it mostly working for s maximizer

--- examples/simulacra_tmp.py
+++ examples/simulacra_tmp.py
@@ -12,12 +12,12 @@ from trlx.data.configs import TRLConfig
 
 if __name__ == "__main__":
     def reward_fn(list_of_str):
-        return [s.count('s')/math.sqrt(len(s)) for s in list_of_str]
+        return [s.count('s') for s in list_of_str]
 
 
     trlx.train(
         "EleutherAI/gpt-j-6b",
         reward_fn=reward_fn,
         config=TRLConfig.load_yaml("configs/ppo_gptj.yml"),
-        #eval_prompts=["She sells"] * 4,
+        eval_prompts=["She sells"],
     )
