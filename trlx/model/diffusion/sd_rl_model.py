d6d395b87beaefdf3b0d146b9a2b449c1e003910
shahbuland <smatiana1@gmail.com>
2023-01-21 15:26:03 -0500

    Start work on models for rl-diffusers

--- trlx/model/diffusion/sd_rl_model.py
+++ trlx/model/diffusion/sd_rl_model.py
@@ -6,14 +6,14 @@ from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 from trlx.data.configs import TRLConfig
 from trlx.data.diffusion_types import DiffPPORLElement, DiffPPORLBatch
 from trlx.model import register_model
-from trlx.model.accelerate_base_model import AccelerateRLModel
+from trlx.model import BaseRLModel
 from trlx.pipeline.diffusion_ppo_pipeline import DiffusionPPORolloutStorage
 from trlx.model.nn.sd_models import UNetActorCritic
 
 @register_model
-class AccelerateSDModel(AccelerateRLModel):
-    def __init__(self, config : TRLConfig):
-        super().__init__(config)
+class SDModel(BaseRLModel):
+    def __init__(self, config : TRLConfig, train_mode = True):
+        super().__init__(config, train_mode)
 
         model_id = config.model.model_path
 
@@ -22,7 +22,7 @@ class AccelerateSDModel(AccelerateRLModel):
         pipe = pipe.to("cuda")
         cfg = pipe.unet.config
 
-        self.model = UNetActorCritic(cfg, pipe.vae_scale_factor, config.method.img_size)
+        self.model = UNetActorCritic(pipe.vae, pipe.unet)
 
         # Pipe has:
         # - tokenizer (transformers.CLIPTokenizer)
@@ -30,10 +30,11 @@ class AccelerateSDModel(AccelerateRLModel):
         # - unet (diffusers.UNet2DConditionModel)
         #   - forward([b, c, h, w] : sample, [t] : timestep, [b, n, d] : encoder hidden, return_dict = True)
         #   - return:  Unet2DConditionOutput([b, c, h, w] : sample)
-        # - vae (diffusers.AutoEncoderML)
+        # - vae (diffusers.AutoEncoderKL)
         #   - forward([b, c, h, w] : sample, return_dict = True)
         #   - return:  DecoderOoutput([b, c, h, w] : sample)
-
+        #   - encode([b, c, h, w])
+        #   - return distribution
         self.store = DiffusionPPORolloutStorage()
 
         rollout_loader = self.store.create_loader(
