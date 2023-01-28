a9913aaeca89006bebffce1d3210325121d3704f
jon-tow <jonathantow1@gmail.com>
2023-01-28 04:59:03 +0000

    Revert newline deletion

--- trlx/trainer/nn/ppo_models.py
+++ trlx/trainer/nn/ppo_models.py
@@ -73,29 +73,41 @@ class FixedKLController:
 class PPOConfig(MethodConfig):
     """
     Config for PPO method
+
     :param ppo_epochs: Number of updates per batch
     :type ppo_epochs: int
+
     :param num_rollouts: Number  of experiences to observe before learning
     :type num_rollouts: int
+
     :param init_kl_coef: Initial value for KL coefficient
     :type init_kl_coef: float
+
     :param target: Target value for KL coefficient
     :type target: float
+
     :param horizon: Number of steps for KL coefficient to reach target
     :type horizon: int
+
     :param gamma: Discount factor
     :type gamma: float
+
     :param lam: GAE lambda
     :type lam: float
+
     :param cliprange: Clipping range for PPO policy loss (1 - cliprange, 1 + cliprange)
     :type cliprange: float
+
     :param cliprange_value: Clipping range for predicted values
                             (observed values - cliprange_value, observed values + cliprange_value)
     :type cliprange_value: float
+
     :param vf_coef: Value loss scale w.r.t policy loss
     :type vf_coef: float
+
     :param gen_kwargs: Additioanl kwargs for the generation
     :type gen_kwargs: Dict[str, Any]
+
     :param gen_experience_kwargs: if this is not None, then the experience is generated using this
     :type gen_experience_kwargs: Dict[str, Any]
     """
