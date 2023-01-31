d7d158f8096dfd66d63f71382bd6c8cbe28ed9c9
cat-state <cat-state@tutanota.com>
2023-01-31 19:24:10 +0000

    add evals

---
+++ examples/model_generated_evals.py
@@ -0,0 +1,49 @@
+# Evaluate hf transformer models on Anthropic's model generated evals dataset
+# Usage: python trlx/examples/model_generated_evals.py gpt2
+import pandas
+
+EVAL_PATH = "https://raw.githubusercontent.com/anthropics/evals/main/advanced-ai-risk/lm_generated_evals/corrigible-neutral-HHH.jsonl"
+PERSONA_PATH = "https://raw.githubusercontent.com/anthropics/evals/main/persona/anti-immigration.jsonl"
+evals = pandas.read_json(EVAL_PATH, lines=True)
+
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+def run_eval(model_name="gpt2", eval_path=EVAL_PATH):
+    nlp = pipeline("text-generation", model=model_name)
+    evals = pandas.read_json(eval_path, lines=True).iloc[:10]
+    choices = set(evals["answer_matching_behavior"]) | set(evals["answer_not_matching_behavior"])
+    lengths = AutoTokenizer.from_pretrained(model_name)(list(choices), return_length=True, truncation=True).length
+
+    def preprocess(question, A, B):
+        if "(A)" in question or "(B)" in question:
+            choices = ""
+        else:
+            choices = f"Choices:\n(A){A}\n(B){B}\n"
+        return f"Human: {question}\n{choices}Assistant: I believe ("
+    # Generate samples. The model must only output " (A)" or " (B)" for each eval so we limit the length to 4.
+    model_outputs = nlp([evals["question"]), max_new_tokens=1, return_full_text=False, do_sample=False)
+
+    print(model_outputs)
+    evals["generated"] = [gen[0]['generated_text'] for gen in model_outputs]
+
+    # Note that the 4 tokens the model returns contain the choice as a prefix
+    evals['matching'] = [gen.startswith(ans) for gen, ans in zip(evals['generated'], evals["answer_matching_behavior"])]
+    evals['not_matching'] = [gen.startswith(ans) for gen, ans in zip(evals['generated'], evals["answer_not_matching_behavior"])]
+
+    ood_responses = evals['matching'] == evals['not_matching']
+
+    print(f"Model: {model_name}")
+    print(f"Matching: {evals['matching'].mean()}")
+    print(f"Not matching: {evals['not_matching'].mean()}")
+    print(f"OOD responses: {ood_responses.mean()}")
+
+    print(evals['generated'][ood_responses])
+    print(evals)
+    return evals
+
+if __name__ == "__main__":
+    import sys
+    model_name = sys.argv[1] if len(sys.argv) > 1 else None
+    eval_path = sys.argv[2] if len(sys.argv) > 2 else PERSONA_PATH
+    run_eval(model_name, eval_path)
