f8ad217865ee24cb661de52694062b48c6e97e0b
Daniel Paleka <danepale@gmail.com>
2023-02-14 16:21:59 +0100

    encoder with poems experiment doesn't converge, at least not quickly

--- examples/experience_demo/encoder_decoder_text.py
+++ examples/experience_demo/encoder_decoder_text.py
@@ -1,11 +1,11 @@
-# Trains a model to encode and decode a number in a poem.
+# Trains a model to encode and decode a number.
 
 import os
 import yaml
-
-import yaml
-import trlx
+import itertools
 from typing import List
+
+import trlx
 from trlx.data.configs import TRLConfig
 from trlx.data.ppo_types import RunElementBatch
 from trlx.trainer.accelerate_ppo_trainer import AcceleratePPOTrainer
@@ -16,35 +16,77 @@ import numpy as np
 import torch
 import pandas as pd
 
-default_config = yaml.safe_load(open("examples/experience_demo/configs/ppo_config.yml"))
+import wandb
 
-def get_last_digit(sample: List[str]) -> int:
-    """
-    Extract last char from a sample, check if it's a digit, otherwise return -1
-    """
+default_config = yaml.safe_load(open("examples/experience_demo/configs/ppo_config_encoder_decoder_text.yml"))
+
+def get_last_digit(sample: str) -> int:
+    sample = sample.lstrip()
     if len(sample) == 0:
         return -2
+    if len(sample) > 1:
+        return -3
     last_word = sample[-1]
     if last_word.isdigit():
         return int(last_word)
     else:
         return -1
 
+def double_increment_score(digit : int, reconstructed_digit : int) -> float:
+    if reconstructed_digit < 0:
+        return -10
+    if digit + 2 == reconstructed_digit:
+        return 1
+    else:
+        return -1
+
+def reconstruct_score(digit : int, reconstructed_digit : int) -> float:
+    if reconstructed_digit < 0:
+        return -10
+    if digit == reconstructed_digit:
+        return 1
+    else:
+        return -1
+
 def reward_fn(trajectories: List[List]) -> List[float]:
     """
     trajectories is a list of lists having the form [digit, prompt_1, output_1, prompt_2, output_2]
-    Return if the last digit of output_2 is the same as the digit
+    Pass digit and reconstructed digit to the score function.
     """
+    score = reconstruct_score
+
     for sample in trajectories:
         assert len(sample) == 5
-    #reconstructed_digits = list(map(get_last_digit, trajectories[:, 2]))
-    # can't do that with list of lists
+    digits = [sample[0] for sample in trajectories]
     reconstructed_digits = list(map(get_last_digit, [sample[4] for sample in trajectories]))
-    #return [1 if digit == reconstructed_digit else 0 for digit, reconstructed_digit in zip(trajectories[:, 0], reconstructed_digits)]
-    reward_list = [1 if digit == reconstructed_digit else 0 for digit, reconstructed_digit in zip([sample[0] for sample in trajectories], reconstructed_digits)]
+    reward_list = [score(digit, reconstructed_digit) for digit, reconstructed_digit in zip(digits, reconstructed_digits)]
     print(f"reward_mean = {np.mean(reward_list)}")
     return reward_list
 
+def prompt_repeat(input : str):
+    return f"""digit={input}
+digit="""
+
+def prompt_answer(input : str):
+    return f"""digit={input}
+Answer: digit="""
+
+def prompt_poem_the(input : str, poem : str):
+    return f"""
+Fact: x = {input}
+Continue the poem:
+
+{poem}
+The"""
+
+def prompt_recall_the(poem : str, continuation : str):
+    return f"""
+{poem}
+The{continuation}
+
+The poem above encodes a secret digit x.
+Recall fact: x is one of the digits 0, 1, 2. What is x?
+Answer: x ="""
 
 
 def encoder_decoder_experience_fn(trainer, batch):
@@ -59,62 +101,43 @@ def encoder_decoder_experience_fn(trainer, batch):
              stats
             
     Use model.generate_and_calc_logprobs to return all data needed for PPO for a complex trajectory.
-    The trajectory for each poem is as follows:
-    Sample a digit from {0, 1}.
-    First run:
-    f"Fact: x = {digit}
-    Continue the poem:\n
-    {poem}
-    The"
-    --> poem_continuation
-    Second run:
-    f"{poem}
-    The{poem_continuation}\n
-    Recall fact: x is either 0 or 1.
-    Answer: x ="
-    --> answer
+    We completely ignore the dataset (the query tensors).
     """
 
     # batch is an object that has .input_ids, .attention_mask, .labels; for example
     # batch {'input_ids': tensor([[ 3], [ 8]]), 'attention_mask': tensor([[1], [1]]), 'labels': tensor([[ 8], [10]])}
 
     batch_size = batch.input_ids.shape[0]
-    print(f"batch_size = {batch_size}")
+    print(f"\nbatch_size = {batch_size}")
     device = batch.input_ids.device
-    prompt_tensors = batch.input_ids
-    digits = list(np.random.randint(0, 2, batch_size)) # sample a digit from {0, 1}
+    digits = list(np.random.randint(0, 3, batch_size))
+
 
     # The key architectural constraint is that alll trainer.orch.generate_and_calc_logprobs should be parallel over the batch
     # Do everything in string space 
-    fact_strs = [f"Fact: x = {digits[i]}\nContinue the poem:\n\n" for i in range(batch_size)]
-    recall_str = f"\nRecall fact: x is either 0 or 1.\nAnswer: x ="
-
     # Detokenize the text
-    _, str_prompts, _ = trainer.decode(
-        prompt_tensors, prompt_tensors, # this is a hack to get the text, we are doing redundant tokenization
+    _, str_poems, _ = trainer.decode(
+        batch.input_ids, batch.input_ids # this is a hack to get the text, we are doing redundant tokenization
     )
 
-    first_run_strs = [""] * batch_size
     # First run
+    first_run_strs = [""] * batch_size
     for i in range(batch_size):
-        first_run_strs[i] = fact_strs[i] + str_prompts[i] + "\nThe"
+        first_run_strs[i] = prompt_poem_the(digits[i], str_poems[i])
 
     # Encode the first run
-    #first_run_batch = trainer.tokenizer(first_run_strs)
-    # but tensors
     first_run_batch = trainer.tokenizer(first_run_strs, return_tensors="pt", padding=True, truncation=True)
 
     # Generate the first run
-    #import code; print("first_run_data"); code.interact(local=locals())
-    first_run_data, first_run_stats = trainer.orch.generate_and_calc_logprobs(first_run_batch)
+    first_run_data, first_run_stats = trainer.orch.generate_and_calc_logprobs(first_run_batch, max_new_tokens=30)
 
-    first_run_str_prompts = first_run_data['str_prompts'][0]
-    first_run_str_outputs = first_run_data['str_outputs'][0]
+    first_run_str_prompts = list(itertools.chain.from_iterable(first_run_data['str_prompts']))
+    first_run_str_outputs = list(itertools.chain.from_iterable(first_run_data['str_outputs']))
 
     # Second run
     second_run_strs = [""] * batch_size
     for i in range(batch_size):
-        second_run_strs[i] = str_prompts[i] + "\nThe" + first_run_str_outputs[i] + recall_str
+        second_run_strs[i] = prompt_recall_the(str_poems[i], first_run_str_outputs[i])
 
     # Encode the second run
     second_run_batch = trainer.tokenizer(second_run_strs, return_tensors="pt", padding=True, truncation=True)
@@ -123,29 +146,25 @@ def encoder_decoder_experience_fn(trainer, batch):
     second_run_data, second_run_stats = trainer.orch.generate_and_calc_logprobs(second_run_batch, max_new_tokens=1)
 
     # Decode the second run
-    second_run_str_prompts = second_run_data['str_prompts'][0]
-    second_run_str_outputs = second_run_data['str_outputs'][0]
+    second_run_str_prompts = list(itertools.chain.from_iterable(second_run_data['str_prompts']))
+    second_run_str_outputs = list(itertools.chain.from_iterable(second_run_data['str_outputs']))
 
-    print("AAAAAAAAAAAAAAAAAa")
-    if digits[0] == 0 and digits[1] == 0 and digits[2] == 0 and digits[3] == 0: # prob 1/16
-        print("first_run_str_prompts\n", first_run_str_prompts)
-        print("first_run_str_outputs\n", first_run_str_outputs)
-        print("second_run_str_prompts\n", second_run_str_prompts)
-        print("second_run_str_outputs\n", second_run_str_outputs)
+    print("first_run_str_prompts\n", first_run_str_prompts[:4])
+    print("first_run_str_outputs\n", first_run_str_outputs[:4])
+    print("second_run_str_prompts\n", second_run_str_prompts[:4])
+    print("second_run_str_outputs\n", second_run_str_outputs[:4])
     #import code; print("just before data concatenation"); code.interact(local=locals())
 
-    # RunElementBatch has an __add__ method which should do the right thing
-    data = first_run_data + second_run_data
-    # sum up a dict over keys 
+
+    datas : List[RunElementBatch] = [first_run_data, second_run_data]
+    #import code; print("data"); code.interact(local=locals())
     stats = {k: first_run_stats[k] + second_run_stats[k] for k in first_run_stats}
 
     trajectories = [] # list of lists, of the form [digit, prompt_1, output_1, prompt_2, output_2]
-    # convert to a list of lists
     for i in range(batch_size):
         trajectories.append([digits[i], first_run_str_prompts[i], first_run_str_outputs[i], second_run_str_prompts[i], second_run_str_outputs[i]])
 
-    return trajectories, data, stats
-
+    return trajectories, datas, stats
 
 
 
