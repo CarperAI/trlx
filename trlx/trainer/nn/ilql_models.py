43571a54f3417d92b9ff6581b7e859efc734a2d4
Duy Phung <duyphung@ip-172-31-45-16.ec2.internal>
2023-02-15 18:30:35 +0000

    fix typo

--- trlx/trainer/nn/ilql_models.py
+++ trlx/trainer/nn/ilql_models.py
@@ -438,7 +438,7 @@ class Seq2SeqWithValueHeads(nn.Module):
         samples = input_ids.clone()
         max_new_tokens = min(max_new_tokens, max_length - input_ids.shape[1])
         if decoder_input_ids is None:
-            decoder_input_ids = input_ids.zeros(input_ids.shape[0], 1, dtype=torch.long, device=input_ids.device)
+            decoder_input_ids = input_ids.new_zeros(input_ids.shape[0], 1)
 
         finished = torch.zeros(input_ids.shape[0], 1, dtype=torch.long, device=input_ids.device)
         for _ in range(max_new_tokens):
