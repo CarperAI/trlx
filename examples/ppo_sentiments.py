5324b2df06345292d7f83c6339c50e087655a0fb
reciprocated <56548574+reciprocated@users.noreply.github.com>
2023-02-18 23:11:52 +0200

    feat(ppo_sentiments): batch_size=12, #eval_prompts=109

--- examples/ppo_sentiments.py
+++ examples/ppo_sentiments.py
@@ -52,7 +52,7 @@ def main(hparams={}):
     trlx.train(
         reward_fn=reward_fn,
         prompts=prompts,
-        eval_prompts=["I don't know much about Hungarian underground"] * 64,
+        eval_prompts=["I don't know much about Hungarian underground"] * 109,
         config=config,
     )
 
