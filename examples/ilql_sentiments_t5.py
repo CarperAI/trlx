7a9e06c1b2d94eba6299c6559579d2c8ef9c66d5
Duy Phung <duyphung@ip-26-0-128-55.ec2.internal>
2023-02-28 07:15:27 +0000

    add ppo sent t5

--- examples/ilql_sentiments_t5.py
+++ examples/ilql_sentiments_t5.py
@@ -1,5 +1,4 @@
 import os
-import pathlib
 from typing import Dict, List
 
 import numpy as np
@@ -28,7 +27,7 @@ def get_positive_score(scores):
     return dict(map(lambda x: tuple(x.values()), scores))["POSITIVE"]
 
 
-config_path = pathlib.Path(__file__).parent.joinpath("../configs/ilql_config.yml")
+config_path = "configs/ilql_config.yml"
 with config_path.open() as f:
     default_config = yaml.safe_load(f)
 
