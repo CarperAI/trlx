a50512b326fee37d91df8da115420f62fbbd7217
Duy Phung <duyphung@cw-prod-login-0.cw-prod-login.tenant-stabilitytraining-704a100.svc.tenant.chi.local>
2023-03-04 04:34:25 +0000

    move config to main ppo t5 sent

--- examples/ppo_sentiments_t5.py
+++ examples/ppo_sentiments_t5.py
@@ -15,11 +15,80 @@ def get_positive_score(scores):
     "Extract value associated with a positive sentiment from pipeline's output"
     return dict(map(lambda x: tuple(x.values()), scores))["POSITIVE"]
 
+from trlx.data.configs import (
+    ModelConfig,
+    OptimizerConfig,
+    SchedulerConfig,
+    TokenizerConfig,
+    TrainConfig,
+    TRLConfig,
+)
+from trlx.models.modeling_ppo import PPOConfig
 
-config_path = pathlib.Path("configs/ppo_config_t5.yml")
-with config_path.open() as f:
-    default_config = yaml.safe_load(f)
 
+default_config = TRLConfig(
+    train=TrainConfig(
+        seq_length=128,
+        epochs=100,
+        total_steps=100000,
+        batch_size=12,
+        checkpoint_interval=10000,
+        eval_interval=100,
+        pipeline="PromptPipeline",
+        trainer="AcceleratePPOTrainer",
+        save_best=False
+    ),
+    model=ModelConfig(
+        model_path="lvwerra/t5-imdb",
+        num_layers_unfrozen=-1,
+        model_arch_type="seq2seq",
+    ),
+    tokenizer=TokenizerConfig(
+        tokenizer_path="lvwerra/t5-imdb",
+        truncation_side="right",
+    ),
+    optimizer=OptimizerConfig(
+        name="adamw",
+        kwargs={
+            "lr": 5.0e-5,
+            "betas": [0.9, 0.999],
+            "eps": 1.0e-8,
+            "weight_decay": 1.0e-6,
+        },
+    ),
+    scheduler=SchedulerConfig(
+        name="cosine_annealing",
+        kwargs={
+            "T_max": 100000,
+            "eta_min": 5.0e-5,
+        },
+    ),
+    method=PPOConfig(
+        name="PPOConfig",
+        num_rollouts=128,
+        chunk_size=12,
+        ppo_epochs=4,
+        init_kl_coef=0.05,
+        target=6,
+        horizon=10000,
+        gamma=0.99,
+        lam=0.95,
+        cliprange=0.2,
+        cliprange_value=0.2,
+        vf_coef=1,
+        scale_reward=None,
+        ref_mean=None,
+        ref_std=None,
+        cliprange_reward=10,
+        gen_kwargs={
+            "max_new_tokens": 50,
+            "do_sample": True,
+            "top_k": 0,
+            "top_p": 1,
+            "eos_token_id": -1,
+        },
+    ),
+)
 
 class LengthSampler:
     """
@@ -59,6 +128,7 @@ def main(hparams={}):
         input_size = LengthSampler(input_min_text_length, input_max_text_length)
 
         def tokenize(sample):
+            sample['review'] = sample['review'].replace("/>br", "")
             sample["input_ids"] = tokenizer.encode(sample["review"])[: input_size()] + [tokenizer.eos_token_id]
             sample["query"] = tokenizer.decode(sample["input_ids"])
             return sample
@@ -76,6 +146,7 @@ def main(hparams={}):
         input_size = LengthSampler(input_min_text_length, input_max_text_length)
 
         def tokenize(sample):
+            sample['review'] = sample['review'].replace("/>br", "")
             sample["input_ids"] = tokenizer.encode(sample["review"])[: input_size()] + [tokenizer.eos_token_id]
             sample["query"] = tokenizer.decode(sample["input_ids"])
             return sample
