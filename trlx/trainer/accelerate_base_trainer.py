7224397fa5b1fcf10f4a5b341691da24a556ea69
jon-tow <jonathantow1@gmail.com>
2023-03-08 15:57:13 +0000

    Left pad ckpt iter count for view sort

--- trlx/trainer/accelerate_base_trainer.py
+++ trlx/trainer/accelerate_base_trainer.py
@@ -487,7 +487,7 @@ class AccelerateRLTrainer(BaseRLTrainer):
                     self.iter_count += 1
 
                     if self.iter_count % self.config.train.checkpoint_interval == 0:
-                        subfolder = f"checkpoint_{self.iter_count}"
+                        subfolder = f"checkpoint_{self.iter_count:0{len(str(self.total_steps))}d}"
                         directory = os.path.join(self.config.train.checkpoint_dir, subfolder)
                         self.save(directory)
 
@@ -535,7 +535,7 @@ class AccelerateRLTrainer(BaseRLTrainer):
                     tbar.update()
 
                     if self.iter_count >= self.total_steps:
-                        subfolder = f"checkpoint_{self.iter_count}"
+                        subfolder = f"checkpoint_{self.iter_count:0{len(str(self.total_steps))}d}"# Left pad the dir name with zeros
                         directory = os.path.join(self.config.train.checkpoint_dir, subfolder)
                         self.save(directory)
                         return self.evaluate()
