642a48f8d8660bce4139ab7ca3043d73e28fcea2
Alex Muzio <alferre@microsoft.com>
2023-03-23 00:37:05 +0000

    Adding metric_fn to ppo translation example

--- examples/ppo_translation_t5.py
+++ examples/ppo_translation_t5.py
@@ -22,7 +22,6 @@ except ImportError:
         "To run this example, please install the `evaluate` and `nltk` packages" "by running `pip install evaluate`"
     )
 
-import torch
 
 config = TRLConfig(
     train=TrainConfig(
@@ -34,7 +33,7 @@ config = TRLConfig(
         eval_interval=500,
         pipeline="PromptPipeline",
         trainer="AcceleratePPOTrainer",
-        tracker='wandb'
+        tracker="wandb"
         # tracker=None
     ),
     model=ModelConfig(
@@ -93,43 +92,74 @@ config = TRLConfig(
 )
 
 
-comet_metric = evaluate.load('comet', 'wmt20-comet-da')
+comet_metric = evaluate.load("comet", "wmt20-comet-da")
+bleu_metric = evaluate.load("bleu")
+chrf_metric = evaluate.load("chrf")
 
 
 if __name__ == "__main__":
 
-    def reward_fn(samples: List[str], prompts: List[str], outputs: List[str]):
+    def reward_fn(samples: List[str], prompts: List[str], outputs: List[str]) -> List[float]:
         # WHAT should samples be for translation?
         original_sents = [translation_map[prompt.strip()] for prompt in prompts]
 
         scores = comet_metric.compute(
-                    predictions=[output.strip() for output in outputs],
-                    references=[original['tgt'] for original in original_sents],
-                    sources=[original['src'] for original in original_sents])["scores"]
-
+            predictions=[output.strip() for output in outputs],
+            references=[original["tgt"] for original in original_sents],
+            sources=[original["src"] for original in original_sents],
+        )["scores"]
         return scores
 
+    def metric_fn(samples: List[str], prompts: List[str], outputs: List[str]) -> List[float]:
+        # Compute BLEU and CHRF
+        original_sents = [translation_map[prompt.strip()] for prompt in prompts]
 
-    train_dataset = load_dataset("wmt16", "de-en", split='train', cache_dir="/home/aiscuser/dev/trlx_mrt/examples/notebooks/data", streaming=True)
-    valid_dataset = load_dataset("wmt16", "de-en", split='validation', cache_dir="/home/aiscuser/dev/trlx_mrt/examples/notebooks/data", streaming=False)
+        comet_score = comet_metric.compute(
+            predictions=[output.strip() for output in outputs],
+            references=[original["tgt"] for original in original_sents],
+            sources=[original["src"] for original in original_sents],
+        )['mean_score']
 
+        bleu_score = bleu_metric.compute(
+            predictions=[output.strip() for output in outputs],
+            references=[original["tgt"] for original in original_sents],
+        )['bleu']
 
-    src_lang = 'en'
-    tgt_lang = 'de'
+        chrf_score = bleu_metric.compute(
+            predictions=[output.strip() for output in outputs],
+            references=[original["tgt"] for original in original_sents],
+        )['score']
+
+        return {
+            'bleu': bleu_score,
+            'chrf': chrf_score,
+            'comet': comet_score
+        }
+
+    train_dataset = load_dataset(
+        "wmt16", "de-en", split="train", cache_dir="/home/aiscuser/dev/trlx_mrt/examples/notebooks/data", streaming=True
+    )
+    valid_dataset = load_dataset(
+        "wmt16",
+        "de-en",
+        split="validation",
+        cache_dir="/home/aiscuser/dev/trlx_mrt/examples/notebooks/data",
+        streaming=False,
+    )
+
+    src_lang = "en"
+    tgt_lang = "de"
     PREFIX = "translate English to German: "
 
-
     # take 20,000 samples from the training set as prompts for training
-    original_src_dataset = [sent_pair['translation'][src_lang] for sent_pair in train_dataset.take(20000)]
-    tgt_dataset = [sent_pair['translation'][tgt_lang] for sent_pair in train_dataset.take(20000)]
+    original_src_dataset = [sent_pair["translation"][src_lang] for sent_pair in train_dataset.take(20000)]
+    tgt_dataset = [sent_pair["translation"][tgt_lang] for sent_pair in train_dataset.take(20000)]
     src_dataset = [PREFIX + src_sent for src_sent in original_src_dataset]
 
-
     # take 1,000 samples from the validation set as prompts for evaluation
-    val_original_src_dataset = [sent_pair[src_lang] for sent_pair in valid_dataset['translation'][0:1000]]
-    val_tgt_dataset = [sent_pair[tgt_lang] for sent_pair in valid_dataset['translation'][0:1000]]
+    val_original_src_dataset = [sent_pair[src_lang] for sent_pair in valid_dataset["translation"][0:1000]]
+    val_tgt_dataset = [sent_pair[tgt_lang] for sent_pair in valid_dataset["translation"][0:1000]]
     val_src_dataset = [PREFIX + src_sent for src_sent in val_original_src_dataset]
-
 
     # make dictionary of prompts and labels to use for reward function
     tokenizer = AutoTokenizer.from_pretrained(config.model.model_path)
@@ -139,25 +169,25 @@ if __name__ == "__main__":
     max_length = config.train.seq_length - config.method.gen_kwargs["max_new_tokens"]
     translation_map = {}
 
-
     for i in tqdm(range(len(original_src_dataset))):
         key = tokenizer.decode(
             tokenizer(src_dataset[i], truncation=True, max_length=max_length, add_special_tokens=False)["input_ids"],
             skip_special_tokens=True,
         )  # get prompt like trlx's prompt
-        translation_map[key.strip()] = {'src': original_src_dataset[i], 'tgt': tgt_dataset[i]}
-
+        translation_map[key.strip()] = {"src": original_src_dataset[i], "tgt": tgt_dataset[i]}
 
     for i in tqdm(range(len(val_original_src_dataset))):
         key = tokenizer.decode(
-            tokenizer(val_src_dataset[i], truncation=True, max_length=max_length, add_special_tokens=False)["input_ids"],
+            tokenizer(val_src_dataset[i], truncation=True, max_length=max_length, add_special_tokens=False)[
+                "input_ids"
+            ],
             skip_special_tokens=True,
         )  # get prompt like trlx's prompt
-        translation_map[key.strip()] = {'src': val_original_src_dataset[i], 'tgt': val_tgt_dataset[i]}
-
+        translation_map[key.strip()] = {"src": val_original_src_dataset[i], "tgt": val_tgt_dataset[i]}
 
     trlx.train(
         reward_fn=reward_fn,
+        metric_fn=metric_fn,
         prompts=src_dataset,
         eval_prompts=val_src_dataset,
         config=config,
