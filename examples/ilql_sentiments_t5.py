852dfee561e3f7d13cec030bf3a6d40c5100c84b
cat-state <meow@cat.cat>
2023-03-27 16:53:15 +0100

    make t5 deterministic

--- examples/ilql_sentiments_t5.py
+++ examples/ilql_sentiments_t5.py
@@ -81,9 +81,10 @@ class LengthSampler:
 
     def __init__(self, min_value, max_value):
         self.values = list(range(min_value, max_value))
+        self.rng = np.random.default_rng(seed=2023)
 
     def __call__(self):
-        return np.random.choice(self.values)
+        return self.rng.choice(self.values)
 
 
 def main(hparams={}):
