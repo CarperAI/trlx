a43e68b44fe1c7d245ec2c8546f0ec7b8e0ad34c
Nakarin <5127052+nakarin@users.noreply.github.com>
2023-03-28 05:36:53 +0700

    Create ilql_th_sentiments.py

---
+++ examples/ilql_th_sentiments.py
@@ -0,0 +1,50 @@
+import json
+import os
+import sys
+from typing import Dict, List
+
+from datasets import load_dataset
+from transformers import pipeline
+
+import trlx
+from trlx.data.default_configs import TRLConfig, default_ilql_config
+
+
+def get_positive_score(scores):
+    "Extract value associated with a positive sentiment from pipeline's output"
+    return dict(map(lambda x: tuple(x.values()), scores))["POSITIVE"]
+
+
+def main(hparams={}):
+    # Merge sweep config with default config if given
+    config = TRLConfig.update(default_ilql_config().to_dict(), hparams)
+    config.model.model_path = 'nakcnx/OTG-Math-680'
+    config.tokenizer.tokenizer_path = 'nakcnx/OTG-Math-680' #'nakcnx/TGPT-2-345M' #'nakcnx/TGPT-Neo-125M'
+
+    sentiment_fn = pipeline(
+        "sentiment-analysis",
+        "poom-sci/WangchanBERTa-finetuned-sentiment",
+        top_k=3,
+        truncation=True,
+        batch_size=128, #256,
+        device=0 if int(os.environ.get("LOCAL_RANK", 0)) == 0 else -1,
+    )
+
+    def metric_fn(samples: List[str], **kwargs) -> Dict[str, List[float]]:
+        sentiments = list(map(get_positive_score, sentiment_fn(samples)))
+        return {"sentiments": sentiments}
+
+    imdb = load_dataset("nakcnx/Thai-IMDB")
+
+    trlx.train(
+        samples=imdb['train']["review_th_pythainlp"],
+        rewards=imdb['train']["sentiment"],
+        eval_prompts=["หนังเรื่องไม่สนุกเลย"] * 32, #64,
+        metric_fn=metric_fn,
+        config=config,
+    )
+
+
+if __name__ == "__main__":
+    hparams = {} if len(sys.argv) == 1 else json.loads(sys.argv[1])
+    main(hparams)
