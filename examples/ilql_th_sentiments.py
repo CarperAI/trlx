7ad2a713fe696e83747ff17e640ecbbc871bd418
Nakarin <5127052+nakarin@users.noreply.github.com>
2023-03-28 06:20:37 +0700

    Update ilql_th_sentiments.py

--- examples/ilql_th_sentiments.py
+++ examples/ilql_th_sentiments.py
@@ -4,7 +4,9 @@ import sys
 from typing import Dict, List
 
 from datasets import load_dataset
+from datasets import Dataset
 from transformers import pipeline
+import pandas as pd
 
 import trlx
 from trlx.data.default_configs import TRLConfig, default_ilql_config
@@ -35,10 +37,12 @@ def main(hparams={}):
         return {"sentiments": sentiments}
 
     imdb = load_dataset("nakcnx/Thai-IMDB")
-
+    df = pd.DataFrame( imdb['train'] )
+    df = df.dropna()
+    imdb2 = Dataset.from_pandas(df)
     trlx.train(
-        samples=imdb['train']["review_th_pythainlp"],
-        rewards=imdb['train']["sentiment"],
+        samples=imdb2["review_th_pythainlp"],
+        rewards=imdb2["sentiment"],
         eval_prompts=["หนังเรื่องไม่สนุกเลย"] * 32, #64,
         metric_fn=metric_fn,
         config=config,
