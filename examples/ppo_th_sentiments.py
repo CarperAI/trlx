714b4eb212932a049cda2137c96c8acc87abf537
Nakarin <5127052+nakarin@users.noreply.github.com>
2023-03-28 04:45:24 +0700

    Create ppo_th_sentiments.py

---
+++ examples/ppo_th_sentiments.py
@@ -0,0 +1,60 @@
+# Generates positive movie reviews by tuning a pretrained model on IMDB dataset
+# with a sentiment reward function
+import json
+import os
+import sys
+from typing import List
+
+import torch
+from datasets import load_dataset
+from transformers import pipeline
+
+import trlx
+from trlx.data.default_configs import TRLConfig, default_ppo_config
+
+
+def get_positive_score(scores):
+    "Extract value associated with a positive sentiment from pipeline's output"
+    return dict(map(lambda x: tuple(x.values()), scores))["pos"]
+
+
+def main(hparams={}):
+    # Merge sweep config with default config if given
+    config = TRLConfig.update(default_ppo_config().to_dict(), hparams)
+    config['model']['model_path'] = 'nakcnx/OTG-Math-680'
+    config['tokenizer']['tokenizer_path'] = 'nakcnx/OTG-Math-680' #'nakcnx/TGPT-2-345M' #'nakcnx/TGPT-Neo-125M'
+
+    if torch.cuda.is_available():
+        device = int(os.environ.get("LOCAL_RANK", 0))
+    else:
+        device = -1
+
+    sentiment_fn = pipeline(
+        "sentiment-analysis",
+        "poom-sci/WangchanBERTa-finetuned-sentiment",
+        top_k=3,
+        truncation=True,
+        batch_size=256,
+        device=device,
+    )
+
+    def reward_fn(samples: List[str], **kwargs) -> List[float]:
+        sentiments = list(map(get_positive_score, sentiment_fn(samples)))
+        print(f"sentiments: {sentiments}")
+        return sentiments
+
+    # Take few words off of movies reviews as prompts
+    imdb = load_dataset("nakcnx/Thai-IMDB")
+    prompts = [" ".join(review.split()[:4]) for review in imdb["review_th"]]
+
+    trlx.train(
+        reward_fn=reward_fn,
+        prompts=prompts,
+        eval_prompts=["หนังเรื่องไม่สนุกเลย"] * 64,
+        config=config,
+    )
+
+
+if __name__ == "__main__":
+    hparams = {} if len(sys.argv) == 1 else json.loads(sys.argv[1])
+    main(hparams)
