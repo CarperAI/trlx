ab9516e1c6fbbf3628983b2bdb293d8802ffdfa7
nakarin <nakarinl@ais.co.th>
2023-03-29 05:50:27 +0700

    Change Chunk_size

--- examples/ppo_th_sentiments.py
+++ examples/ppo_th_sentiments.py
@@ -25,6 +25,7 @@ def main(hparams={}):
     config = TRLConfig.update(default_ppo_config().to_dict(), hparams)
     config.model.model_path = 'nakcnx/OTG-Math-680'
     config.tokenizer.tokenizer_path = 'nakcnx/OTG-Math-680' #'nakcnx/TGPT-2-345M' #'nakcnx/TGPT-Neo-125M'
+    config.chunk_size = 32
 
     if torch.cuda.is_available():
         device = int(os.environ.get("LOCAL_RANK", 0))
@@ -37,7 +38,6 @@ def main(hparams={}):
         top_k=3,
         truncation=True,
         batch_size=64, #256
-        chunk_size=32,
         device=device,
     )
 
