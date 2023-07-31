8b6a6fc1a82e69f46220c34a74a4615797062216
maxjeblick <maximilianjeblick@gmail.com>
2023-07-31 14:49:19 +0200

    fix reward model state dict loading

--- examples/summarize_rlhf/trlx_gptj_text_summarization.py
+++ examples/summarize_rlhf/trlx_gptj_text_summarization.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     rw_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
     rw_tokenizer.pad_token = rw_tokenizer.eos_token
     rw_model = GPTRewardModel(SFT_MODEL_PATH)
-    rw_model.load_state_dict(torch.load(REWARD_CHECKPOINT_PATH))
+    rw_model.load_state_dict(torch.load(REWARD_CHECKPOINT_PATH), strict=False)
     rw_model.half()
     rw_model.eval()
     rw_device = torch.device("cuda:{}".format(1))  # set reward model device
