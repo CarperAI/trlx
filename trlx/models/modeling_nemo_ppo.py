4de0cc1902745e36540c48757499879ef409dc3e
cat-state <cat@meow>
2023-07-31 22:43:14 +0100

    strict=True

--- trlx/models/modeling_nemo_ppo.py
+++ trlx/models/modeling_nemo_ppo.py
@@ -467,7 +467,7 @@ class PPOGPT(MegatronGPTModel):
 
         lm_state_dict = {**lm_state_dict, "encoder": encoder_state_dict}
 
-        unwrap_float16_module(self.model).load_state_dict(lm_state_dict, strict=False)
+        unwrap_float16_module(self.model).load_state_dict(lm_state_dict, strict=True)
         print(f"Loaded from pretrained {rank_params}")
 
     def model_provider_func(self, pre_process: bool, post_process: bool):
--- trlx/models/modeling_nemo_sft.py
+++ trlx/models/modeling_nemo_sft.py
@@ -122,7 +122,7 @@ class SFTGPT(MegatronGPTModel):
 
         lm_state_dict = {**lm_state_dict, "encoder": encoder_state_dict}
 
-        unwrap_float16_module(self.model).load_state_dict(lm_state_dict, strict=False)
+        unwrap_float16_module(self.model).load_state_dict(lm_state_dict, strict=True)
         print(f"Loaded from pretrained {rank_params}")
 
     # Adapted from NeMo
